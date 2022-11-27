3bee6e693f1943a0f427af77cf5e978f2bbc2443
Bernhard Bablok <bablokb@gmx.de>
2022-11-27 18:59:13 +0100

    added slidesho-mode

--- src/main.py
+++ src/main.py
@@ -6,6 +6,11 @@
 #
 # Other hardware requires other pins and probably a different display-driver
 #
+# The program has two modes: an initial slideshow-mode and the final
+# keys-mode (HID). The intial mode is used to obfuscate the main purpose
+# of the gadget. If the user presses the enter-button during display
+# of a predefined image, the device switches from slideshow to keys-mode.
+#
 # Author: Bernhard Bablok
 # License: GPL3
 #
@@ -27,13 +32,14 @@ from adafruit_bitmap_font import bitmap_font
 # key-library
 import keypad
 
-# HID-libraries
-import usb_hid
-from adafruit_hid.keyboard  import Keyboard
-from keyboard_layout_win_de import KeyboardLayout
-
 # application library
 from SecretsManager import SecretsManager as SecretsManager
+
+# --- configuration   --------------------------------------------------------
+
+IMAGES    = ['image00.bmp','image01.bmp','image02.bmp',
+             'image03.bmp','image04.bmp']
+KEY_IMAGE = 2
 
 # SPI/TFT pins
 PIN_MOSI = board.GP11
@@ -54,18 +60,18 @@ PIN_USER  = board.GP15
 PIN_PW    = board.GP17
 PIN_UP    = board.GP2
 PIN_DOWN  = board.GP18
+PIN_ENTER = board.GP3
 
 # unused pins
 PIN_LEFT  = board.GP16
 PIN_RIGHT = board.GP20
-PIN_ENTER = board.GP3
 
 # fonts and colors
-FONT   = bitmap_font.load_font("fonts/DejaVuSansMono-Bold-24.bdf")
-BLACK  = 0x000000
-WHITE  = 0xFFFFFF
-RED    = 0xFF0000
-GREEN  = 0x00FF00
+FONT            = bitmap_font.load_font("fonts/DejaVuSansMono-Bold-24.bdf")
+BG_COLOR        = 0x000000      # black
+SITE_COLOR      = 0xFFFFFF      # white
+USERNAME_COLOR  = 0xFF0000      # red
+PASSWORD_COLOR  = 0x00FF00      # green
 
 # keep a small gap at the border of the display
 GAP =  2
@@ -80,7 +86,6 @@ class App:
     """ constructor """
 
     self._setup_hw()
-    self._setup_hid()
 
     # helper for layout
     width  = self._display.width
@@ -96,11 +101,9 @@ class App:
 
     self._secrets = SecretsManager()
     self._fields  = []
+    self._img_nr  = 0
     self._group   = displayio.Group()
-    self._background()
-    self._create_fields()
-    self._update_fields(self._secrets.current())
-    self._display.show(self._group)
+    self._update_image()
 
   # --- hardware-setup   ----------------------------------------------------
 
@@ -115,12 +118,14 @@ class App:
                             colstart=28,rowstart=0,invert=True,
                             rotation=TFT_ROTATE,bgr=TFT_BGR)
 
-    keys = keypad.Keys([PIN_USER,PIN_PW,PIN_UP,PIN_DOWN],
+    keys = keypad.Keys([PIN_USER,PIN_PW,PIN_UP,PIN_DOWN,PIN_ENTER],
                        value_when_pressed=False,pull=True,
                        interval=0.1,max_events=4)
     self._key_events = keys.events
+    self._switch_on = KEY_IMAGE
     self._key_callbacks = [
-      self._on_user,self._on_pw,self._on_up,self._on_down
+      self._on_img_prev,self._on_img_next,self._on_img_prev,
+      self._on_img_next,self._on_img_select
     ]
 
   # --- HID-setup   ----------------------------------------------------------
@@ -128,16 +133,35 @@ class App:
   def _setup_hid(self):
     """ setup HID-keyboard """
 
+    # HID-libraries
+    import usb_hid
+    from adafruit_hid.keyboard  import Keyboard
+    from keyboard_layout_win_de import KeyboardLayout
+
     keyboard       = Keyboard(usb_hid.devices)
     self._keyboard = KeyboardLayout(keyboard)
+
+  # --- switch to keys-mode   ------------------------------------------------
+
+  def _switch_mode(self):
+    """ switch to keys-mode """
+
+    self._setup_hid()
+    self._key_callbacks = [
+      self._on_user,self._on_pw,self._on_up,self._on_down,self._noop
+    ]
+    del self._group[0]
+    self._background()
+    self._create_fields()
+    self._update_fields(self._secrets.current())
 
   # --- create background   --------------------------------------------------
 
   def _background(self):
-    """ all black background """
+    """ all bg_color background """
 
     palette    = displayio.Palette(1)
-    palette[0] = BLACK
+    palette[0] = BG_COLOR
     background = vectorio.Rectangle(pixel_shader=palette,
                                     width=self._display.width,
                                     height=self._display.height+4, x=0, y=0)
@@ -159,7 +183,7 @@ class App:
   def _create_fields(self):
     """ create text fields for site, user, pw """
 
-    colors = [WHITE,RED,GREEN]
+    colors = [SITE_COLOR,USERNAME_COLOR,PASSWORD_COLOR]
     pos    = ['NW','W','SW']
     for i in range(3):
       field = self._create_text(pos[i],colors[i],"xxxxxxxxxxxxxxxxxx")
@@ -172,6 +196,55 @@ class App:
 
     for i in range(3):
       self._fields[i].text = item[i]
+
+  # --- update image on the screen   -----------------------------------------
+
+  def _update_image(self):
+    img_file = open(IMAGES[self._img_nr], "rb")
+    bmp  = displayio.OnDiskBitmap(img_file)
+    tile = displayio.TileGrid(bmp,pixel_shader=displayio.ColorConverter())
+
+    if len(self._group) == 1:
+      del self._group[0]
+    self._group.append(tile)
+    self._display.show(self._group)
+    self._display.refresh()
+
+  # --- empty callback   -----------------------------------------------------
+
+  def _noop(self):
+    """ empty callback """
+    pass
+
+  # --- callback for enter-key   ---------------------------------------------
+
+  def _on_img_select(self):
+    """ select image and switch mode """
+
+    if self._img_nr == self._switch_on:
+      self._switch_mode()
+    else:
+      # we allow no error (this effectively disables a mode-switch)
+      KEY_IMAGE = -1
+
+  # --- callback for user-key/up-key in slideshow-mode   ---------------------
+
+  def _on_img_prev(self):
+    """ select previous image """
+
+    if self._img_nr == 0:
+      self._img_nr = len(IMAGES) - 1
+    else:
+      self._img_nr = self._img_nr - 1
+    self._update_image()
+
+  # --- callback for pw-key/down-key in slideshow-mode   ---------------------
+
+  def _on_img_next(self):
+    """ select next image """
+
+    self._img_nr = (self._img_nr + 1) % len(IMAGES)
+    self._update_image()
 
   # --- callback for user-key   ----------------------------------------------
 
